b9aaf60844e9a2f4f34f4089316bd1d0f8cf585a
darioflo <darioflo19@icloud.com>
2025-04-04 12:05:35 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}", // Asegúrate que incluya esto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
