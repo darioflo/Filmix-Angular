78daadf16ad8f206d33978d719be39ba05ee5ccb
darioflo <darioflo19@icloud.com>
2025-04-05 14:09:38 -0600

    Rutas Listas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { SeriesComponent } from './routes/series/series.component';
+import { MyListComponent } from './routes/my-list/my-list.component';
+import { CategoriesComponent } from './routes/categories/categories.component';
+import { FilmsComponent } from './routes/films/films.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: 'series', component: SeriesComponent },
+  { path: 'my-list', component: MyListComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
