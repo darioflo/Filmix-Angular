6b9961dc553355a89974a6afe4afdc6e15fb7377
darioflo <darioflo19@icloud.com>
2025-04-06 18:15:43 -0600

    Pelis terminadas

--- src/app/models/FilmPopular.ts
+++ src/app/models/FilmPopular.ts
@@ -1,7 +1,7 @@
 export default interface FilmPopular {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: [number, number, number, number];
+  genre_ids: number[];
   id: number;
   original_language: string;
   original_title: string;
--- src/app/routes/films/films.component.ts
+++ src/app/routes/films/films.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, input, OnInit } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FilmServicesService } from '../../services/film-services.service';
+import FilmHome from '../../models/FilmHome';
+import { CardFilmComponent } from '../../components/card-film/card-film.component';
+import { NgFor } from '@angular/common';
+import FilmPopular from '../../models/FilmPopular';
 
 @Component({
   selector: 'app-films',
-  imports: [],
+  imports: [NavbarComponent, CardFilmComponent, NgFor],
   templateUrl: './films.component.html',
-  styleUrl: './films.component.css'
+  styleUrl: './films.component.css',
 })
-export class FilmsComponent {
+export class FilmsComponent implements OnInit {
+  page: number = 1;
+  fimlServices = inject(FilmServicesService);
+  films: FilmPopular[] = [];
+  film = input<FilmPopular>();
+  isLoading: boolean = false;
 
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const scrollLimit = document.body.offsetHeight;
+
+    if (scrollPosition > scrollLimit - 100) {
+      this.page++;
+      this.loadMovies();
+    }
+  }
+  loadMovies(): void {
+    this.isLoading = true;
+    this.fimlServices.getMovie(this.page).subscribe({
+      next: (data) => {
+        this.films = [...this.films, ...data.results];
+        console.log(this.films);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
